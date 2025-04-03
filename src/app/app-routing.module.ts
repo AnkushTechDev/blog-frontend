a1a868892112385d463690493e3dac98506e10bd
Ankush Jain <akshay.jain521@gmail.com>
2025-04-03 18:13:22 +0530

    Added auth guard for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,23 @@
 import { PostsDetailsComponent } from './posts/posts-details/posts-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
-
 import { PostsComponent } from './posts/posts.component';
 import { UsersComponent } from './users/users.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
-
+import { AuthGuard } from './auth.guard';
 
 const appRoutes: Routes = [
     { path: "", component: DashboardComponent },
-    { path: "dashboard", component: DashboardComponent },
-    { path: "posts", component: PostsComponent },
-    { path: "posts/:id/details", component: PostsDetailsComponent },
+    { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }, // Protect this route
+    { path: "posts", component: PostsComponent, canActivate: [AuthGuard] }, // Protect this route
+    { path: "posts/:id/details", component: PostsDetailsComponent, canActivate: [AuthGuard] }, // Protect this route
     { path: "users", component: UsersComponent },
     { path: "users/:id/details", component: UserDetailsComponent },
     { path: "login", component: LoginComponent },
-    { path: "profile", component: ProfileComponent },
+    { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }, // Protect this route
 ];
 
 @NgModule({
